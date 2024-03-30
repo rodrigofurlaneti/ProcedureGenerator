bd6e91f51507769f66c1e6196f5630c669e50f13
rodrigofurlaneti <rodrigofurlaneti31@hotmail.com>
2024-03-30 12:52:04 -0300

    Adicionar arquivos de projeto.

---
+++ ProcedureGenerator.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProcedureGenerator.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
